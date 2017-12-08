a6fc0f1ab76df645c633b658227e3b795dd1922e
Bryan Downs <xerobasix@gmail.com>
2017-12-08 12:27:04 -0700

    create class outline

---
+++ Experimenter.py
@@ -0,0 +1,41 @@
+'''
+Experimenter.py is a driver class that intiates 25 experiments using 5 data sets clustered
+with 5 algorithms
+'''
+
+'''
+get_data relies on the external class datahandler to read in csv files of the 5 datasets
+used in the experiments. 
+'''
+def get_data():
+    pass
+
+'''
+qac_exp executes a quick ant-colony optimization experiment
+'''
+def qac_exp():
+    pass
+
+'''
+hpso_exp executes a hierarchical pso clustering experiment 
+'''
+def hpso_exp():
+    pass
+
+'''
+dbscan_exp executes a dbscan clustering experiment
+'''
+def dbscan_exp():
+    pass
+
+'''
+kmeans_exp executes a kmeans clustering experiment
+'''
+def kmeans_exp():
+    pass
+
+'''
+cnn_exp executes a competative neural network experiment
+'''
+def cnn_exp():
+    pass
