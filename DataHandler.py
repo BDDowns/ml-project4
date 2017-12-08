57aae9cda4e9bb0d127d0b7ab3ebeecb637afe3c
Bryan Downs <xerobasix@gmail.com>
2017-12-08 12:35:07 -0700

    make class with csv read fn

--- DataHandler.py
+++ DataHandler.py
@@ -1,3 +1,15 @@
 '''
 Data Handler preprocesses data for clustering experiments
 '''
+
+import numpy as np
+import pandas as pd
+
+
+class DataHandler():
+    def __init__(self):
+        pass
+
+def handleCSV(self, file):
+    df = pd.read_csv(file)
+    return df
