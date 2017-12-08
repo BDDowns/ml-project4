2c787976d7f89a9a1eb661c010bb231acc4444b5
Bryan Downs <xerobasix@gmail.com>
2017-12-08 12:15:32 -0700

    add function outline

--- Clustering.py
+++ Clustering.py
@@ -2,3 +2,52 @@
 Clustering.py Performs experiments using 5 clustering algorithms on 5 data sets
 for a total of 25 experiments. 
 '''
+
+class Clustering():
+    def __init__(self):
+        pass
+
+
+'''
+dbscan performs a clustering of data using the DBSCAN algorithm
+@param
+@return 
+'''
+def dbscan():
+    pass
+
+
+'''
+kmeans performs a clustering of data using the kmeans algorithm
+@param
+@return
+'''
+def kmeans():
+    pass
+
+
+'''
+complearning performs a clustering of input data using competative learning neural networks
+@param
+@return
+'''
+def complearning():
+    pass
+
+
+'''
+aco peforms a clustering of input data using the ant colony optomization algorithm
+@param
+@return
+'''
+def aco():
+    pass
+
+
+'''
+pso performs a clustering of input date using particle swarm optimization
+@param
+@return
+'''
+def pso():
+    pass
